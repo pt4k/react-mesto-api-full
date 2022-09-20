7e84abd78252e3b3b5165a3b5795548266a1704e
Yuriy Pishchenko <locbne@gmail.com>
2022-09-20 20:11:03 +0300

    исправил ошибку mixed content2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,7 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
 const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
+  'http://pishchenko.mesto.students.nomoredomains.sbs',
+  'https://pishchenko.mesto.students.nomoredomains.sbs',
   'localhost:3000',
 ];
 
